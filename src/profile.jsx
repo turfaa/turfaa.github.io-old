042734f493f5cfaf3fea6734b355ea53f88e8871
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2016-09-24 06:30:00 +0700

    Fix link bug

--- src/profile.jsx
+++ src/profile.jsx
@@ -4,7 +4,7 @@ export class Profile extends React.Component{
   constructor(props, context){
     super(props, context);
 
-    this.state = {name : "Turfa Auliarachman", nim : "13515133", major : "Informatics Engineering", university : "Bandung Institute of Technology", foto : "img/pasfoto.jpg", birthdate : "April 9, 1997", email : "turfa_auliarachman@rocketmail.com", projecteuler : "kingfalcon", alamat : "Jl. Cisitu Lama V 42B, Dago, Coblong, Bandung", facebook : "turfa.auliarachman", gplus : "TurfaAuliarachman", twitter : "TurfaGreenbat", github : "turfaa", codeforces : "NangisDarah"};
+    this.state = {name : "Turfa Auliarachman", nim : "13515133", major : "Informatics Engineering", university : "Bandung Institute of Technology", foto : "img/pasfoto.jpg", birthdate : "April 9, 1997", email : "turfa_auliarachman@rocketmail.com", projecteuler : "kingfalcon", alamat : "Jl. Cisitu Lama V 42B, Dago, Coblong, Bandung", facebook : "https://facebook.com/turfa.auliarachman", gplus : "https://plus.google.com/+TurfaAuliarachman", twitter : "https://twitter.com/TurfaGreenbat", github : "https://github.com/turfaa", codeforces : "http://codeforces.com/NangisDarah"};
   }
 
   render() {
@@ -34,9 +34,9 @@ export class Profile extends React.Component{
               <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span className="caret"></span><span className="sr-only">Social</span></button>
 
               <ul className="dropdown-menu" role="menu">
-                <li><a href="https://www.facebook.com/{this.state.facebook}" target="_blank">Facebook</a></li>
-                <li><a href="https://plus.google.com/+{this.state.gplus}" target="_blank">Google+</a></li>
-                <li><a href="https://twitter.com/{this.state.twitter}" target="_blank">Twitter</a></li>
+                <li><a href={this.state.facebook} target="_blank">Facebook</a></li>
+                <li><a href={this.state.gplus} target="_blank">Google+</a></li>
+                <li><a href={this.state.twitter} target="_blank">Twitter</a></li>
               </ul>
             </div>
 
@@ -45,8 +45,8 @@ export class Profile extends React.Component{
               <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span className="caret"></span><span className="sr-only">Computer Science Things</span></button>
 
               <ul className="dropdown-menu" role="menu">
-                <li><a href="https://www.github.com/{this.state.github}" target="_blank">Github</a></li>
-                <li><a href="http://codeforces.com/profile/{this.state.codeforces}" target="_blank">Codeforces</a></li>
+                <li><a href={this.state.github} target="_blank">Github</a></li>
+                <li><a href={this.state.codeforces} target="_blank">Codeforces</a></li>
               </ul>
             </div>
           </div>
