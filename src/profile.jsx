42bc409a8ebc4d60af6e9b261f2f0dac0385fbfe
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2016-09-23 23:55:29 +0700

    Minor unimportant changes

--- src/profile.jsx
+++ src/profile.jsx
@@ -4,7 +4,7 @@ export class Profile extends React.Component{
   constructor(props, context){
     super(props, context);
 
-    this.state = {name : "Turfa Auliarachman", nim : "13515133", major : "Informatics Engineering", university : "Bandung Institute of Technology", foto : "img/pasfoto.jpg"};
+    this.state = {name : "Turfa Auliarachman", nim : "13515133", major : "Informatics Engineering", university : "Bandung Institute of Technology", foto : "img/pasfoto.jpg", birthdate : "April 9, 1997", email : "turfa_auliarachman@rocketmail.com", projecteuler : "kingfalcon", alamat : "Jl. Cisitu Lama V 42B, Dago, Coblong, Bandung", facebook : "turfa.auliarachman", gplus : "TurfaAuliarachman", twitter : "TurfaGreenbat", github : "turfaa", codeforces : "NangisDarah"};
   }
 
   render() {
@@ -19,13 +19,13 @@ export class Profile extends React.Component{
             <h4>{this.state.major} | <small>{this.state.university}</small></h4>
 
             <p>
-              <span className="glyphicon glyphicon-gift"></span>April 9, 1997<br />
-              <span className="glyphicon glyphicon-envelope"></span><a href="mailto:turfa_auliarachman@rocketmail.com">turfa_auliarachman@rocketmail.com</a><br />
-              <span className="glyphicon glyphicon-home"  ></span>Jl. Cisitu Lama V 42B, Dago, Coblong, Bandung<br />
+              <span className="glyphicon glyphicon-gift"></span>{this.state.birthdate}<br />
+              <span className="glyphicon glyphicon-envelope"></span><a href="mailto:{this.state.email}">{this.state.email}</a><br />
+              <span className="glyphicon glyphicon-home"  ></span>{this.state.alamat}<br />
             </p>
 
             <p>
-              My Project Euler username is <code>kingfalcon</code><br />
+              My Project Euler username is <code>{this.state.projecteuler}</code><br />
               <cite>Note : You can find me in other sites using this username</cite>
             </p>
 
@@ -34,9 +34,9 @@ export class Profile extends React.Component{
               <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span className="caret"></span><span className="sr-only">Social</span></button>
 
               <ul className="dropdown-menu" role="menu">
-                <li><a href="https://www.facebook.com/turfa.auliarachman" target="_blank">Facebook</a></li>
-                <li><a href="https://plus.google.com/+TurfaAuliarachman" target="_blank">Google+</a></li>
-                <li><a href="https://twitter.com/TurfaGreenbat" target="_blank">Twitter</a></li>
+                <li><a href="https://www.facebook.com/{this.state.facebook}" target="_blank">Facebook</a></li>
+                <li><a href="https://plus.google.com/+{this.state.gplus}" target="_blank">Google+</a></li>
+                <li><a href="https://twitter.com/{this.state.twitter}" target="_blank">Twitter</a></li>
               </ul>
             </div>
 
@@ -45,8 +45,8 @@ export class Profile extends React.Component{
               <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span className="caret"></span><span className="sr-only">Computer Science Things</span></button>
 
               <ul className="dropdown-menu" role="menu">
-                <li><a href="https://www.github.com/turfaa" target="_blank">Github</a></li>
-                <li><a href="http://codeforces.com/profile/NangisDarah" target="_blank">Codeforces</a></li>
+                <li><a href="https://www.github.com/{this.state.github}" target="_blank">Github</a></li>
+                <li><a href="http://codeforces.com/profile/{this.state.codeforces}" target="_blank">Codeforces</a></li>
               </ul>
             </div>
           </div>
