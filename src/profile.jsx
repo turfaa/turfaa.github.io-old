75d03e0d9ed31319a09c781659caf34653f96719
Fahmi <fahmitok123@gmail.com>
2016-10-22 18:58:17 +0700

    make profile component more reusable by passing data from props instead of state

--- src/profile.jsx
+++ src/profile.jsx
@@ -1,31 +1,31 @@
 import React from  'react';
 
 export class Profile extends React.Component{
+
   constructor(props, context){
     super(props, context);
-
-    this.state = {name : "Turfa Auliarachman", nim : "13515133", major : "Informatics Engineering", university : "Bandung Institute of Technology", foto : "img/pasfoto.jpg", birthdate : "April 9, 1997", email : "turfa_auliarachman@rocketmail.com", projecteuler : "kingfalcon", alamat : "Jl. Cisitu Lama V 42B, Dago, Coblong, Bandung", facebook : "https://facebook.com/turfa.auliarachman", gplus : "https://plus.google.com/+TurfaAuliarachman", twitter : "https://twitter.com/TurfaGreenbat", github : "https://github.com/turfaa", codeforces : "http://codeforces.com/profile/NangisDarah"};
-  }
+  };
 
   render() {
+    console.log(this.props);
     return (
       <div style={{'marginBottom' : '10px'}}>
         <div className="row">
           <div className="col-xs-12 col-sm-4">
-            <h3><img src={this.state.foto} style={{'width' : '100%'}}/></h3>
+            <h3><img src={this.props.profileData.foto} style={{'width' : '100%'}}/></h3>
           </div>
           <div className="col-xs-12 col-sm-8">
-            <h3>{this.state.name}</h3>
-            <h4>{this.state.major} | <small>{this.state.university}</small></h4>
+            <h3>{this.props.profileData.name}</h3>
+            <h4>{this.props.profileData.major} | <small>{this.props.profileData.university}</small></h4>
 
             <p>
-              <span className="glyphicon glyphicon-gift"></span>{this.state.birthdate}<br />
-              <span className="glyphicon glyphicon-envelope"></span><a href="mailto:{this.state.email}">{this.state.email}</a><br />
-              <span className="glyphicon glyphicon-home"  ></span>{this.state.alamat}<br />
+              <span className="glyphicon glyphicon-gift"></span>{this.props.profileData.birthdate}<br />
+              <span className="glyphicon glyphicon-envelope"></span><a href="mailto:{this.props.profileData.email}">{this.props.profileData.email}</a><br />
+              <span className="glyphicon glyphicon-home"  ></span>{this.props.profileData.alamat}<br />
             </p>
 
             <p>
-              My Project Euler username is <code>{this.state.projecteuler}</code><br />
+              My Project Euler username is <code>{this.props.profileData.projecteuler}</code><br />
               <cite>Note : You can find me in other sites using this username</cite>
             </p>
 
@@ -34,9 +34,9 @@ export class Profile extends React.Component{
               <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span className="caret"></span><span className="sr-only">Social</span></button>
 
               <ul className="dropdown-menu" role="menu">
-                <li><a href={this.state.facebook} target="_blank">Facebook</a></li>
-                <li><a href={this.state.gplus} target="_blank">Google+</a></li>
-                <li><a href={this.state.twitter} target="_blank">Twitter</a></li>
+                <li><a href={this.props.profileData.facebook} target="_blank">Facebook</a></li>
+                <li><a href={this.props.profileData.gplus} target="_blank">Google+</a></li>
+                <li><a href={this.props.profileData.twitter} target="_blank">Twitter</a></li>
               </ul>
             </div>
 
@@ -45,8 +45,8 @@ export class Profile extends React.Component{
               <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span className="caret"></span><span className="sr-only">Computer Science Things</span></button>
 
               <ul className="dropdown-menu" role="menu">
-                <li><a href={this.state.github} target="_blank">Github</a></li>
-                <li><a href={this.state.codeforces} target="_blank">Codeforces</a></li>
+                <li><a href={this.props.profileData.github} target="_blank">Github</a></li>
+                <li><a href={this.props.profileData.codeforces} target="_blank">Codeforces</a></li>
               </ul>
             </div>
           </div>
