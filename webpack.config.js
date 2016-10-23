f4374e45bb99a2371118afe4e772467d66569844
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2016-10-23 14:25:48 +0700

    Automatically minimize the .js file

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,10 @@ var config = {
       plugins:[
               new webpack.DefinePlugin({
                         'process.env':{'NODE_ENV': JSON.stringify('production')}
-                      })
+                      }),
+              
+              new webpack.optimize.UglifyJsPlugin(),
+              new webpack.optimize.OccurenceOrderPlugin()
             ],
       module : {
               loaders : [
